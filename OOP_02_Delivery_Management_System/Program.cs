d977425ec8512be79bd9ac127bbbf05e3a73c2c9
mary-aiad <mary.aid.bariz"gmail.com>
2026-08-21 14:11:09 +0200

    Add project files.

---
+++ OOP_02_Delivery_Management_System/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP_02_Delivery_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question_01
+            ///A- What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            //Answer (A): Only the copy will be modified; the original won't change because it's a value type, so it's just a copy of the original.
+            ///B- What happens when a Customer variable is copied into another variable and one variable modifies the object 
+            //Answer (B): the object will be modified because the class is a reference type so the 2 variables with the same referance.
+            #endregion
+        }
+    }
+}
